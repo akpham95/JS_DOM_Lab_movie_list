deb06d4d3012fd6362c5e7fc17c32edb6882aa9b
Anthony Pham <akpham95@gmail.com>
2022-09-28 15:55:47 -0500

    Core lab finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let message = document.querySelector('#message')
 document.querySelector("form").addEventListener("submit", addMovie);
 
 function addMovie(event) {
@@ -9,6 +10,7 @@ function addMovie(event) {
     let movie = document.createElement("li")
     let movieTitle = document.createElement("span")
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie)
     movie.appendChild(movieTitle)
 
     const deleteBtn = document.createElement("button")
@@ -22,6 +24,16 @@ function addMovie(event) {
 }
 
 function deleteMovie(event){
+    message.textContent = 'Movie Deleted!'
     event.target.parentNode.remove() 
 }
 
+function crossOffMovie(event){
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked') === true) {
+        message.textContent = 'Movie watched!'
+    }else{
+        message.textContent = 'Movie added back!'
+    }
+}
